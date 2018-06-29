4494e792ec0cd6f4d1d13945e5c44d54d615350e
Judson Neer <judson.neer@everyonecounts.com>
2018-06-29 10:38:58 -0700

    Initial commit of code.

---
+++ main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+
+# Standard library imports
+import datetime
+import logging
+
+# Additional library imports
+import bottle
+
+
+# Configure the logging module
+logformat = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
+logging.basicConfig(format=logformat, level=logging.INFO)
+
+
+# If a semaphore is not released within this time in seconds, it is automatically released
+DEFAULT_TIMEOUT = 300
+
+
+# Semaphore data is stored in memory
+SEMAPHORES = {}
+
+
+@bottle.get('/<semaphoreid>')
+def _getsemaphore(semaphoreid):
+    """
+    Acquire a semphore; if the ID does not exist, it is created.
+    @param semaphoreid: Uniquely identifies the particular semaphore to acquire
+    @return: 204 NO CONTENT if the semaphore is acquired successfully
+             403 FORBIDDEN if the semaphore is not available
+             500 INTERNAL SERVER ERROR if an unknown problem occurs
+    """
+    semaphore = SEMAPHORES.get(semaphoreid, {'id': semaphoreid, 'expiry': datetime.datetime.min})
+    SEMAPHORES[semaphoreid] = semaphore
+    currenttime = datetime.datetime.now()
+    if currenttime > semaphore['expiry']:
+        timeout = bottle.request.get('timeout', DEFAULT_TIMEOUT)
+        semaphore['expiry'] = currenttime + datetime.timedelta(seconds=timeout)
+        bottle.response.status = 204
+    else:
+        bottle.abort(403, 'Semaphore "{0}" is not available'.format(semaphoreid))
+
+
+@bottle.delete('/<semaphoreid>')
+def _deletesemaphore(semaphoreid):
+    """
+    Release a semphore.
+    @param semaphoreid: Uniquely identifies the particular semaphore to release
+    @return: 204 NO CONTENT if the semaphore is released successfully
+             403 FORBIDDEN if the caller has not yet acquired the semaphore
+             404 NOT FOUND if the semaphore does not exist
+             500 INTERNAL SERVER ERROR if an unknown problem occurs
+    """
+    try:
+        del SEMAPHORES[semaphoreid]
+        bottle.response.status = 204
+    except KeyError:
+        bottle.abort(404, 'Semaphore "{0}" does not exist'.format(semaphoreid))
+
+
+@bottle.error(400)
+@bottle.error(403)
+@bottle.error(404)
+@bottle.error(500)
+def _error(error):
+    """
+    Return an error message to clients.
+    @param error: The error message from an above function
+    @return: The appropriate error message
+    """
+    bottle.response.set_header('Cache-Control', 'no-cache')
+    bottle.response.set_header('Content-Type', 'text/plain')
+    return error.body
+
+
+# Start the HTTP server
+bottle.run(host='0.0.0.0', quiet=True)
